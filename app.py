842863890d37d4a9287e25d81281a5264ff4eeaa
caseyjohnsonwv <caseyjohnsonwv@gmail.com>
2020-01-27 18:44:49 -0500

    remove sqlalchemy

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import env
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.update(
     SECRET_KEY = env.APP_SECRET_KEY,
-    SQLALCHEMY_DATABASE_URL = env.DATABASE_URL
 )
-db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def home():
