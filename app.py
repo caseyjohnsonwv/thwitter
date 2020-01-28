41d979c41f55213b3876a123b6ecc7891b9ac5d3
Casey Johnson <ctj0001@mix.wvu.edu>
2020-01-28 14:43:54 -0500

    oauth - creds only saved in session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import env
 from worker import *
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
+import tweepy
 
 app = Flask(__name__)
 app.config.update(
@@ -19,6 +20,38 @@ def go():
     tweets = make_thread(text, preserve_whitespace)
     data = {'tweets':tweets}
     return render_template('index.html', data=data, page='go')
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    try:
+        session['AUTH_TOKEN']
+        session['AUTH_TOKEN_SECRET']
+        redirect_url = '/share'
+    except Exception:
+        auth = tweepy.OAuthHandler(env.TWITTER_API_KEY, env.TWITTER_API_SECRET, env.CALLBACK_URL)
+        redirect_url = auth.get_authorization_url()
+        session['REQUEST_TOKEN'] = auth.request_token
+    finally:
+        return redirect(redirect_url)
+
+@app.route('/callback', methods=['GET'])
+def callback():
+    auth = tweepy.OAuthHandler(env.TWITTER_API_KEY, env.TWITTER_API_SECRET)
+    auth.request_token = session['REQUEST_TOKEN']
+    verifier = request.args.get('oauth_verifier')
+    try:
+        auth.get_access_token(verifier)
+        session['AUTH_TOKEN'],session['AUTH_TOKEN_SECRET'] = auth.access_token, auth.access_token_secret
+        redirect_url = '/share'
+    except Exception:
+        redirect_url = '/'
+    finally:
+        return redirect(redirect_url)
+
+@app.route('/share', methods=['GET'])
+def share():
+    data = {}
+    return render_template('index.html', data=data, page='share')
 
 @app.route('/alive', methods=['GET'])
 def alive():
