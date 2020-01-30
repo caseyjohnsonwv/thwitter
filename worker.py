68d58fd89568dbfc9937ecaf0f44aff5d77d81e5
caseyjohnsonwv <ctj0001@mix.wvu.edu>
2020-01-30 13:38:39 -0500

    offloaded oauth to worker.py - crash inside try/except??

--- worker.py
+++ worker.py
@@ -1,4 +1,36 @@
 import re
+import env
+import tweepy
+from flask import session, request
+
+def initiate_oauth():
+    """
+    return: str
+    """
+    try:
+        session['AUTH_TOKEN']
+        session['AUTH_TOKEN_SECRET']
+        redirect_url = '/share'
+    except Exception:
+        auth = tweepy.OAuthHandler(env.TWITTER_API_KEY, env.TWITTER_API_SECRET, env.CALLBACK_URL)
+        redirect_url = auth.get_authorization_url()
+        session['REQUEST_TOKEN'] = auth.request_token
+    return redirect_url
+
+def oauth_callback():
+    """
+    return: str
+    """
+    auth = tweepy.OAuthHandler(env.TWITTER_API_KEY, env.TWITTER_API_SECRET)
+    try:
+        auth.request_token = session['REQUEST_TOKEN']
+        verifier = request.args.get('oauth_verifier')
+        auth.get_access_token(verifier)
+        session['AUTH_TOKEN'],session['AUTH_TOKEN_SECRET'] = auth.access_token, auth.access_token_secret
+        redirect_url = '/share'
+    except Exception:
+        redirect_url = '/'
+    return redirect_url
 
 def make_thread(text, preserve_whitespace=False):
     """
