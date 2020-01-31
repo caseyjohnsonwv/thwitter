ccf729727803873f65b288f57f4c559e96301436
Casey Johnson <ctj0001@mix.wvu.edu>
2020-01-30 21:20:00 -0500

    only strip whitespace if necessary

--- worker.py
+++ worker.py
@@ -56,7 +56,7 @@ def make_thread(text, preserve_whitespace=False):
         # tweet is full - try to break at last punctuation
         elif breakpoint > 0:
             tweet = tweet[:breakpoint]
-            thread.append(tweet.strip())
+            thread.append(tweet)
             tweet = words[i]
             breakpoint = 0
         # no previous punctuation - break with elipsis mid-word
@@ -65,10 +65,13 @@ def make_thread(text, preserve_whitespace=False):
             while len(tweet) > 240:
                 tweet,overflow = tweet[:237],tweet[237:]
                 tweet += "..."
-                thread.append(tweet.strip())
+                thread.append(tweet)
                 tweet = "..." + overflow
         #next word
         i += 1
-    #append leftovers to thread
-    thread.append(tweet.strip())
+    # append leftovers to thread
+    thread.append(tweet)
+    # strip ending whitespaces
+    if not preserve_whitespace:
+        thread = [tweet.strip() for tweet in thread]
     return thread
