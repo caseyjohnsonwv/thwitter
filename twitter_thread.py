30dcfbbe482efed60a0c08a1642475110a3da7da
Casey Johnson <ctj0001@mix.wvu.edu>
2020-01-30 22:37:03 -0500

    corrected no punctuation found case; fixed tweet length

--- twitter_thread.py
+++ twitter_thread.py
@@ -11,16 +11,19 @@ def make_thread(text, preserve_whitespace=False):
     if not preserve_whitespace:
         text = re.sub('\s+',' ',text)
     start = 0
-    while start + 240 < len(text):
-        end = start + 239
+    while start + 280 < len(text):
+        end = start + 279
         # if end points to multiple punctuation marks, don't break here- keep punctuation together
         while end > 0 and text[end] in PUNCTUATION:
             end -= 1
         # backtrack to next viable breakpoint
         while end > 0 and text[end] not in PUNCTUATION:
             end -= 1
-        # need to handle the case of not finding punctuation!
-        tweet = text[start:end+1]
+        if end > 0:
+            tweet = text[start:end+1]
+        else:
+            end = start+277
+            tweet = text[start:end]+"..."
         thread.append(tweet)
         start = end
     # append leftover text to thread
