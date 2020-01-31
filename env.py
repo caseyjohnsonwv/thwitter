4cfd26d105d2efe58dc7b31a9424cde80c23e7bd
Casey Johnson <ctj0001@mix.wvu.edu>
2020-01-30 22:45:09 -0500

    added env.py to working tree; .env still hidden

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+APP_SECRET_KEY = os.environ["APP_SECRET_KEY"]
+CALLBACK_URL = os.environ["CALLBACK_URL"]
+TWITTER_API_KEY = os.environ["TWITTER_API_KEY"]
+TWITTER_API_SECRET = os.environ["TWITTER_API_SECRET"]
